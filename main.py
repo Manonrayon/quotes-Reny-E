16fae25f0ae86ddfeb4d7392b99d2d072db1f6f1
Manon <manon.reny@efrei.net>
2023-05-25 00:51:11 +0200

    Changing into charcaters inputs

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from functions import *
 
+
 def menu():
     print("\n==== Programming Quotes ====")
-    print("1. Random quote")
-    print("2. Display quotes")
-    print("3. Add quotes")
-    print("4. Exit")
+    print("random : Random quote")
+    print("display : Display quotes")
+    print("add : Add a new quote")
+    print("exit : Exit the program")
+
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4): ")
-        
-        if choice == "1":
+        choice = input(">> ")
+
+        if choice == "random":
             print_quote(random_quote(quotes))
-        elif choice == "2":
+        elif choice == "display":
             count = int(input("Enter the number of quotes to display: "))
             display_quotes(quotes, count)
-            view_quotes(quotes)
-        elif choice == "3":
-            add_quote(quotes,filename)
-        elif choice == "4":
+        elif choice == "add":
+            add_quote(quotes, "quotes.txt")
+        elif choice == "exit":
             print("Good bye...")
             break
         else:
             print("Invalid input")
+
 
 if __name__ == "__main__":
     main()
